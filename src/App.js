621372cdeb59631eb43dab3101d4d842c619b6a1
Martin Fan <m1fan@ryerson.ca>
2019-12-30 13:03:58 -0500

    changed app.js, shows header bar for now

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1> Hello World!  </h1>
+        <h1>TY03</h1>
+        <Button>Home</Button>
+        <Button>Team</Button>
+        <Button>Project</Button>
+        <Button>Tutorial</Button>
+        <Button>Contact</Button>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 import { Router } from 'react-router';
 import { render } from 'react-dom';
-import routes from './routes';
-import ReactDOM from 'react-dom';
+import routes from './routes'
 
-const intro = <h1>Hello World!</h1>
+import App from './App';
+import LabelBottomNavigation from './shared/header';
 
-ReactDOM.render(intro, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<LabelBottomNavigation />, document.getElementById('root'));
